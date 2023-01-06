05da2e5a81946cfe6f83abc5dbc58cc23fde4ff5
danieldenton <danielmdenton@gmail.com>
2023-01-06 08:35:49 -0800

    expect exists

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "@cfaester/enzyme-adapter-react-18";
-import { render } from "@testing-library/react";
 import App from "./App";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-test("renders learn react link", () => {
+test("renders nonemty component without crashing", () => {
   const wrapper = shallow(<App />);
+  expect(wrapper.exists()).toBe(true);
 });
