043b9c167bc18d6e6a5f7b6e9fd81524c1a32d0f
danieldenton <danielmdenton@gmail.com>
2023-01-06 08:27:04 -0800

    enzyme wrapper

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "@cfaester/enzyme-adapter-react-18";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-// test("renders learn react link", () => {
-//   const wrapper = shallow()
-// });
+Enzyme.configure({ adapter: new Adapter() });
+
+test("renders learn react link", () => {
+  const wrapper = shallow(<App />);
+});
